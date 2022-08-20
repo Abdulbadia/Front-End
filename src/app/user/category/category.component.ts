dda355cb67b977ff6665e6ded698e8891ac40195
ahmedpop1 <48593343+ahmedpop1@users.noreply.github.com>
2022-08-20 07:43:24 +0200

    category component

--- src/app/user/category/category.component.ts
+++ src/app/user/category/category.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductAPiService } from '../../modules/admin/Shared/product-api.service';
 import { IProduct } from '../../modules/admin/Standers/iproduct';
+import { CategoriesApiService } from '../../modules/admin/Shared/categories-api.service';
+import { Categories } from '../../modules/admin/Standers/categories';
 
 @Component({
   selector: 'app-category',
@@ -10,12 +12,13 @@ import { IProduct } from '../../modules/admin/Standers/iproduct';
 })
 export class CategoryComponent implements OnInit {
 products:IProduct[];
+categories:Categories[] ;
 category:string;
-  constructor(private Ser:ProductAPiService,private router:Router) {
+  constructor(private Ser:ProductAPiService,private router:Router,private catserv:CategoriesApiService) {
 this.category="كل الاصناف";
 
 
-    const observer={
+    const prdobserver={
       next:(data)=>{
     this.products=data;
     console.log(data);
@@ -23,7 +26,16 @@ this.category="كل الاصناف";
       error:(err:Error)=>{
         err.message}
       }
-  this.Ser.get_AllProduct().subscribe(observer);
+    const catobserver={
+      next:(data)=>{
+    this.categories=data;
+    console.log(data);
+      },
+      error:(err:Error)=>{
+        err.message}
+      }
+  this.Ser.get_AllProduct().subscribe(prdobserver);
+  this.catserv.get_AllCata().subscribe(catobserver);
 
   }
 
